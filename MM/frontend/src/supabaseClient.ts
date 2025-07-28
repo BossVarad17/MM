b7d28ed424218d23d6bcfc31d56c02d8ad2c5283
Varad Vekariya <149096166+BossVarad17@users.noreply.github.com>
2025-07-28 22:35:42 +0530

    Delete MM directory

--- MM/frontend/src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'project_url' // Paste your Project URL here
-const supabaseAnonKey = 'anom-key' // Paste your anon key here
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
