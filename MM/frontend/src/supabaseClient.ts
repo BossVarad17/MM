31a5a964d9a04857ea7a05e667241b947a5762d6
Varad Vekariya <149096166+BossVarad17@users.noreply.github.com>
2025-07-28 21:40:20 +0530

    Update supabaseClient.ts

--- MM/frontend/src/supabaseClient.ts
+++ MM/frontend/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_PROJECT_URL' // Paste your Project URL here
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY' // Paste your anon key here
+const supabaseUrl = 'https://oaphmhwtcnpjxoezkorv.supabase.co' // Paste your Project URL here
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hcGhtaHd0Y25wanhvZXprb3J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MTgyNzIsImV4cCI6MjA2OTI5NDI3Mn0.tkAj5pQcIWVAOATDDf6YsPG6aSJVxX4sPFonzbEUm_A' // Paste your anon key here
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
